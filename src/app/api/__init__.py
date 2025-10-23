24e2da454e38b2e92b27fbf3efe3944bc65270c8
Tom Delahaye <delahaye.tom@icloud.com>
2025-10-23 12:35:59 +0200

    push

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from .health import router as health_router
 from .chat import router as chat_router
-from .google import router as google_router
 from .files import router as files_router
 from .fetchallchats import router as fetchallchats_router
 from .fetchchat import router as fetchchat_router
@@ -25,7 +24,6 @@ from .fetchalldeepcourses import router as fetchalldeepcourses_router
 api_router = APIRouter()
 api_router.include_router(health_router)
 api_router.include_router(chat_router)
-api_router.include_router(google_router)
 api_router.include_router(files_router)
 api_router.include_router(fetchallchats_router)
 api_router.include_router(fetchchat_router)
@@ -50,7 +48,6 @@ __all__ = [
     "health_router",
     "chat_router",
     "api_router",
-    "google_router",
     "files_router",
     "fetchallchats_router",
     "fetchchat_router",
